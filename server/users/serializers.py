0712ea7ea50fc5368c9ccb1efb76b7d9221f583e
Данил <zhulkin01@gmail.com>
2024-10-03 09:45:49 +0300

    общие улучшения

---
+++ server/users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Profile
+
+
+class CreateProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('username', 'password',)
