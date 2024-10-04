720433531cbab94a3c7ba3e06735ca8561979ff3
Danil <zhulkin01@gmail.com>
2024-10-05 00:00:26 +0300

    debug true :)

--- server/server/settings.py
+++ server/server/settings.py
@@ -17,7 +17,7 @@ if not SECRET_KEY:
     SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
 
